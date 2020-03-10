dba0e25f8814b23a346493fdf2846b09e8c2c28c
drnam288 <drnam288@gmail.com>
2020-03-10 13:19:08 +0700

    Add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,16 @@ var gulp = require("gulp"),
     pug = require('gulp-pug'),
     browserSync = require("browser-sync"),
     runSequence = require('run-sequence'),
-    useref = require('gulp-useref');
+    useref = require('gulp-useref'),
+    gulpIf = require('gulp-if'),
+    uglify = require('gulp-uglify');
 
 // gulp.task('useref', function(){
-//   return gulp.src('src/*.html')
+//   return gulp.src('dist/*.html')
 //     .pipe(useref())
+//     // .pipe(gulpIf('*.js', uglify()))
+//     // Minifies only if it's a CSS file
+//     .pipe(gulpIf('*.css', cssnano())
 //     .pipe(gulp.dest('dist'))
 // });
 
@@ -28,8 +33,10 @@ gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.{sass,scss}')      // Gets all files ending with
                                              // .scss in app/scss
                                              // and children dirs
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass,
                                              // log errors to console
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'))             // Outputs it in the css folder
     .pipe(browserSync.reload({               // Reloading with Browser Sync
       stream: true
@@ -38,15 +45,17 @@ gulp.task('sass', function() {
 
 gulp.task('pug', function() {
   return gulp.src("src/pug/**/*.pug")
+      .pipe(sourcemaps.init())
       .pipe(pug())
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest("dist/"))
-      .pipe(browserSync.reload({ // Reloading with Browser Sync
+      .pipe(browserSync.reload({
         stream: true
       }));
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function() {
-  gulp.watch('src/scss/**/*.{sass,scss}', ['sass']);
+  gulp.watch('src/scss/**/*.{sass,scss}', ['sass', 'useref']);
   gulp.watch('src/pug/**/*.pug', ['pug', browserSync.reload]);
   gulp.watch('src/js/**/*.js', [browserSync.reload]);
   gulp.watch("src/*.html").on('change', browserSync.reload);
