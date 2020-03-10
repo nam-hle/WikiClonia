bddf2da91de19c36bb29ad1bdae3b13a82af5bde
drnam288 <drnam288@gmail.com>
2020-03-11 00:02:46 +0700

    Add content roughly

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
 var gulp = require("gulp"),
     sass = require("gulp-sass"),
     postcss = require("gulp-postcss"),
-    autoprefixer = require("autoprefixer"),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
     cssnano = require("cssnano"),
     sourcemaps = require("gulp-sourcemaps"),
     pug = require('gulp-pug'),
     browserSync = require("browser-sync"),
     runSequence = require('run-sequence'),
-    useref = require('gulp-useref'),
     gulpIf = require('gulp-if'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin');
 
-// gulp.task('useref', function(){
-//   return gulp.src('dist/*.html')
-//     .pipe(useref())
-//     // .pipe(gulpIf('*.js', uglify()))
-//     // Minifies only if it's a CSS file
-//     .pipe(gulpIf('*.css', cssnano())
-//     .pipe(gulp.dest('dist'))
-// });
+
+gulp.task('images', function(){
+  return gulp.src('src/asset/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('src/asset/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
 
 gulp.task('browserSync', function() {
   browserSync({
@@ -36,6 +40,11 @@ gulp.task('sass', function() {
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass,
                                              // log errors to console
+    .pipe(autoprefixer())
+    // .pipe(cleanCSS({debug: true}, (details) => {
+    //      console.log(`${details.name}: ${details.stats.originalSize}`);
+    //      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    //    }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'))             // Outputs it in the css folder
     .pipe(browserSync.reload({               // Reloading with Browser Sync
@@ -55,11 +64,11 @@ gulp.task('pug', function() {
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function() {
-  gulp.watch('src/scss/**/*.{sass,scss}', ['sass', 'useref']);
+  gulp.watch('src/scss/**/*.{sass,scss}', ['sass']);
   gulp.watch('src/pug/**/*.pug', ['pug', browserSync.reload]);
   gulp.watch('src/js/**/*.js', [browserSync.reload]);
   gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 
-gulp.task('default', ['sass', 'pug', 'browserSync', 'watch']);
+gulp.task('default', ['sass', 'pug', 'images', 'fonts', 'browserSync', 'watch']);
