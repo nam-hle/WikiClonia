2b19da51e37c33225186a34d8626fe2db9235e8f
drnam288 <drnam288@gmail.com>
2020-04-26 14:58:42 +0700

    Resolve conflict

--- src/components/WikiApp/Article/index.js
+++ src/components/WikiApp/Article/index.js
@@ -1,14 +1,10 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { main } from "./../../../wiki_parser";
-// import Sidebar from "./../SideBar";
-// import Reference from "./../Reference";
 import Content from "./../Content";
 import Menu from "./../Menu";
 import Navigation from "./../Navigation";
 import "lazysizes";
-<<<<<<< HEAD
 import useFetch from "./../../../hooks/useFetch.js";
-import Popover from "react-tiny-popover";
 
 // import Sidebar from "./../SideBar";
 // import Reference from "./../Reference";
@@ -17,11 +13,6 @@ export const ImagesContext = React.createContext(null);
 
 const titles = [
   // "Pet_door"
-=======
-export const ImagesContext = React.createContext(null);
-
-const titles = [
->>>>>>> parent of f769b01... Implement useFetch
   "New_York_City",
   "The_Last_Supper_(Leonardo)",
   "Leonardo_da_Vinci",
@@ -31,48 +22,69 @@ const titles = [
 
 const title = titles[Math.floor(Math.random() * titles.length)];
 
+const parseWikiText = response => main(response?.parse?.wikitext?.["*"]);
+
+const buildURL = params =>
+  "https://en.wikipedia.org/w/api.php?" +
+  new URLSearchParams({ ...params, format: "json", origin: "*" });
+
+const pageContentParams = title => ({
+  action: "parse",
+  prop: "wikitext",
+  page: title
+});
+
+const imageParams = (title, limit = 500) => ({
+  action: "query",
+  prop: "imageinfo",
+  titles: title,
+  generator: "images",
+  gimlimit: limit,
+  iiprop: "url|dimensions"
+});
+
+const summaryParams = title => ({
+  action: "query",
+  prop: "extracts",
+  exsentences: 4,
+  exintro: true,
+  explaintext: true,
+  redirects: 1,
+  titles: title
+});
+
 const Article = () => {
-<<<<<<< HEAD
   const [images, setImages] = useState(null);
-  const [content, setContent] = useState(null);
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-  // get content
-  const contentFetch = useFetch(buildURL(contentParams(title)));
+  const [pageContent, setPageContent] = useState(null);
+
+  // get pageContent
+  const pageContentFetcher = useFetch(buildURL(pageContentParams(title)));
 
   useEffect(() => {
-    setContent(parseWikiText(contentFetch.response));
-  }, [contentFetch.response]);
+    setPageContent(parseWikiText(pageContentFetcher.response));
+  }, [pageContentFetcher.response]);
 
   // get images
   const imageFetch = useFetch(buildURL(imageParams(title)));
 
-=======
-  const [images, setImages] = useState({});
-  // const [references, setReferences] = useState([]);
-  const [parsed, setParsed] = useState({});
-  // const title = titles[Math.floor(Math.random() * titles.length)];
-  // title = title || "New_York_City";
-  // title = title || "The_Last_Supper_(Leonardo)";
-  // title = title || "Leonardo_da_Vinci";
-  // title = title || "Mona_Lisa";
+  // const parseWikiText = response => main(response?.parse?.wikitext?.["*"]);
 
-  // get main content
->>>>>>> parent of f769b01... Implement useFetch
   useEffect(() => {
-    var url = `https://en.wikipedia.org/w/api.php?action=parse&page=${title}&format=json&prop=wikitext&origin=*`;
-    fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(_text => {
-        let rawText = _text.parse.wikitext["*"];
-        let result = main(rawText);
-        setParsed(result);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }, []);
+    let imgs = imageFetch.response?.query?.pages,
+      res = {};
+    // return object {filename: {url, width, height}}
+    for (const key in imgs) {
+      let { url, width, height } = imgs[key]?.imageinfo?.[0];
+      res[imgs[key].title] = { url, width, height };
+    }
+    setImages(res);
+  }, [imageFetch.response]);
+
+  const summaryFetch = useFetch(buildURL(summaryParams(title)));
+
+  useEffect(() => {
+    console.log(summaryFetch.response);
+  }, [summaryFetch.response]);
 
   // get references
   // useEffect(() => {
@@ -87,26 +99,6 @@ const Article = () => {
   //   }
   // }, [parsed]);
 
-  // get images
-  useEffect(() => {
-    var url = `https://en.wikipedia.org/w/api.php?action=query&titles=${title}&generator=images&gimlimit=500&prop=imageinfo&iiprop=url|dimensions|mime&format=json&origin=*`;
-    fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(data => {
-        let imgs = data.query.pages,
-          res = {};
-        for (const key in imgs) {
-          res[imgs[key].title] = imgs[key].imageinfo[0];
-        }
-        setImages(res);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <ImagesContext.Provider value={{ images }}>
       <Fragment>
@@ -114,24 +106,15 @@ const Article = () => {
         <div className="article">
           {/*<Sidebar content={parsed.images} images={images} />*/}
           <div className="hero">
-            <Popover
-              isOpen={isPopoverOpen}
-              position={"top"}
-              content={<div>{"Hi! I'm popover content."}</div>}
-            >
-              <div onClick={() => setIsPopoverOpen(!isPopoverOpen)}>
-                Click me!
-              </div>
-            </Popover>
             <div className="hero__title">{title.replace(/_/g, " ")}</div>
             <div className="hero__credit">
               From Wikipedia, the free encyclopedia
             </div>
-            <Content content={parsed.children} />
+            <Content content={pageContent?.children} />
             {/*<Reference {...{ references }} />*/}
           </div>
         </div>
-        <Navigation headings={parsed.headings} />
+        <Navigation headings={pageContent?.headings} />
       </Fragment>
     </ImagesContext.Provider>
   );
