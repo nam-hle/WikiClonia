cd2314612e28391ae2cd124e423c1e7934102398
drnam288 <drnam288@gmail.com>
2020-04-29 11:06:27 +0700

    before push

--- src/components/WikiApp/Article/test.js
+++ src/components/WikiApp/Article/test.js
@@ -2,12 +2,6 @@ import React from "react";
 import Article from "./";
 import renderer from "react-test-renderer";
 
-import Enzyme, { mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import { act } from "react-dom/test-utils";
-
-Enzyme.configure({ adapter: new Adapter() });
-
 const titles = [
   "Pet_door",
   "New_York_City",
