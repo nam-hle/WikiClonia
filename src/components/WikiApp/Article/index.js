86ed388612ec33eb11c11b9e408f058fd00a1246
drnam288 <drnam288@gmail.com>
2020-06-01 15:40:34 +0700

    Adjust reponsive content

--- src/components/WikiApp/Article/index.js
+++ src/components/WikiApp/Article/index.js
@@ -20,7 +20,7 @@ export const ImagesContext = React.createContext(null);
 // ];
 
 const Article = ({ title }) => {
-  title = title || "Pet_door";
+  title = title || "New_York_City";
   const pageContent = usePageContent(title);
   const images = useImages(title);
 
