68421bd3e988a802a5c7a01acb519067834f639a
drnam288 <drnam288@gmail.com>
2020-06-30 19:05:25 +0700

    Add meta viewport

--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -1,11 +1,11 @@
-import "./../../styles/normalize.css";
-import "./../../styles/theme.scss";
-import "./../../styles/style.sass";
-
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./../../components/App";
+
 import { BrowserRouter } from "react-router-dom";
+import "./../../styles/normalize.css";
+import "./../../styles/theme.scss";
+import "./../../styles/style.sass";
 
 ReactDOM.render(
   <BrowserRouter>
